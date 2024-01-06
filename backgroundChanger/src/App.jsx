69471fdaf979f08dbb5de31968f98b154333547d
Harsh Dhama <dhamaharsh9@gmail.com>
2024-01-06 13:27:08 +0530

    completed and deployed

--- backgroundChanger/src/App.jsx
+++ backgroundChanger/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
     <div className="w-full h-screen duration-200"
       style={{ backgroundColor: color }}>
 
-      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+      <div className="fixed flex flex-wrap justify-center bottom-11 inset-x-0 px-2">
         <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-purple-200 px-3 py-2 rounded-3xl">
           <button className="w-16 h-16 rounded-full text-white bg-red-500" onClick={() => setColor("red")}>Red</button>
           <button className="w-16 h-16 rounded-full text-white bg-blue-500" onClick={() => setColor("blue")}>Blue</button>
